8d08d43ae2e00dea1f2e23b1469f6462258dc69d
Matthew Lyons <matthewjosephlyons@gmail.com>
2019-02-19 17:07:38 -0600

    add /db route

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 var express = require("express");
 var app = express();
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 app.set("port", process.env.PORT || 5000);
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(request, response) {
   response.send("Hello World!");
+});
+
+app.get("/db", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/db", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
 app.listen(app.get("port"), function() {
